3e6548c20d8f0d37738d3a38e5081b2354833e27
johnchanguk <johnchang9094@gmail.com>
2017-04-30 17:25:52 +0100

    define and remove stop words

---
+++ playing_with_classifiers.py
@@ -0,0 +1,44 @@
+import nltk
+import random
+from nltk.corpus import movie_reviews
+from nltk.classify.scikitlearn import SklearnClassifier
+import pickle
+
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.svm import SVC, LinearSVC, NuSVC
+
+from nltk.classify import ClassifierI
+from statistics import mode
+# nltk.download('punkt')
+from nltk.tokenize import word_tokenize
+
+short_pos = open("polaritydata/rt-polarity.pos", "r").read()
+short_neg = open("polaritydata/rt-polarity.neg", "r").read()
+
+documents = []
+
+for r in short_pos.split('\n'):
+    documents.append( (r, "pos") )
+
+for r in short_neg.split('\n'):
+    documents.append( (r, "neg") )
+
+all_words = []
+
+short_pos_words = word_tokenize(short_pos)
+short_neg_words = word_tokenize(short_neg)
+
+for w in short_pos_words:
+    all_words.append(w.lower())
+for w in short_neg_words:
+    all_words.append(w.lower())
+
+all_words = nltk.FreqDist(all_words)
+
+common_words = all_words.most_common(20)
+common_words_dictionary = dict(common_words)
+stop_words = list(common_words_dictionary.keys())
+
+word_features = list(all_words.keys())[:3000]
+refined_word_features = [x for x in word_features if x not in stop_words]
