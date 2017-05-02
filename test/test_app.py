51fdbf1a75959cdf2f7967e7920b611c65528494
Ashwini <ashie.oh@gmail.com>
2017-05-02 18:06:56 +0100

    Add new feature test

--- test/test_app.py
+++ test/test_app.py
@@ -1,5 +1,9 @@
 import pytest
 from flask import url_for
+from digitalaffects.app import create_app
+import capybara
+import capybara.dsl
+capybara.app = create_app()
 
 class TestApp:
 
@@ -11,3 +15,10 @@ class TestApp:
     def test_home(self, client):
         res = client.get(url_for('index'))
         assert res.status_code == 200
+
+    def test_search(self, client):
+        capybara.dsl.page.visit('/')
+        capybara.dsl.page.fill_in('userinput', value='brexit')
+        capybara.dsl.page.find('[name=getmood]').click
+        assert capybara.dsl.page.has_current_path('/')
+        # current path should be /json
