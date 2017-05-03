59a20c6de9d589f43f92c27a042d90c174633e09
allthatilk <imogenkutz@hotmail.com>
2017-05-03 15:23:57 +0100

    Fix for cookie bug. Removal of json route and sessions

--- app.py
+++ app.py
@@ -17,31 +17,20 @@ nav.Bar('top', [
 def index():
     return render_template('index.html')
 
-@app.route("/json")
-def json():
-    results = session['results']
-    text = session['text']
-    tweets = session['tweets']
-    values = [results.count('positive'), results.count('negative')]
-    return render_template('results.html', values=values, text=text, tweets=tweets)
-
-@app.route("/results", methods=['POST'])
+@app.route("/results", methods=['POST', 'GET'])
 def results():
     if request.method == 'POST':
         text = request.form['userinput']
         tweets = getTweets(text)
         results = []
+
         for tweet in tweets:
             result = naiveBayesSentimentCalculator(tweet)
             results.append(result)
 
-        session['tweets'] = tweets
-        session['results'] = results
-        session['text'] = text
-        return redirect(url_for('json'))
+        values = [results.count('positive'), results.count('negative')]
+        return render_template('results.html', values=values, text=text, tweets=tweets)
 
-# secret_key for sessions exposed as no sensitive data stored on sessions
 if __name__ == '__main__':
     app.debug = True
-    app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
     app.run()
--- model/twitterAPI.py
+++ model/twitterAPI.py
@@ -3,7 +3,7 @@ import json
 import os
 from apikeys import ckey, csecret, atoken, asecret
 
-def getTweets(query="belieber", lang="en", count="20", result_type="recent", filename="tweets.txt", geocode="53.721247,3.904416,300mi"):
+def getTweets(query="belieber", lang="en", count="100", result_type="recent", filename="tweets.txt", geocode="53.721247,3.904416,300mi"):
     auth = tweepy.OAuthHandler(ckey, csecret)
     auth.set_access_token(atoken, asecret)
 
