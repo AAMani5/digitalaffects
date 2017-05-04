cae94b287be699d894fd07f372ae9126c3c8f1af
Eva Balog <evebalog@netscape.net>
2017-05-04 12:34:35 +0100

    Save trained classifiers

--- model/NB_BernoulliNB_LogisticRegression_classifiers.py
+++ model/NB_BernoulliNB_LogisticRegression_classifiers.py
@@ -32,42 +32,46 @@ for tweet in pos_tweets:
 for tweet in neg_tweets:
     documents.append( (tweet, "negative") )
 
-all_words = []
+# all_words = []
+#
+# short_pos_words = word_tokenize("\n".join(clean_pos_tweetwords))
+# short_neg_words = word_tokenize("\n".join(clean_neg_tweetwords))
+#
+# word_regex = r'^\w+$'
+#
+# def is_word(sample):
+#     match = re.match(word_regex, sample)
+#     if match:
+#         return True
+#     else:
+#         return False
+#
+# for word in short_pos_words:
+#     if is_word(word):
+#         all_words.append(word.lower())
+# for word in short_neg_words:
+#     if is_word(word):
+#         all_words.append(word.lower())
+#
+# all_words = nltk.FreqDist(all_words)
+#
+# common_words = all_words.most_common(20)
+# common_words_dictionary = dict(common_words)
+# stop_words = list(common_words_dictionary.keys())
+#
+#
+# word_features = list(all_words.keys())[:5000]
+# print(word_features)
+# print(stop_words)
+# refined_vocabulary = [word for word in word_features if word not in stop_words]
 
-short_pos_words = word_tokenize("\n".join(clean_pos_tweetwords))
-short_neg_words = word_tokenize("\n".join(clean_neg_tweetwords))
+saved_refined_vocabulary = open("refined_vocabulary.pickle", "rb")
+refined_vocabulary = pickle.load(saved_refined_vocabulary)
+saved_refined_vocabulary.close()
 
-word_regex = r'^\w+$'
-
-def is_word(sample):
-    match = re.match(word_regex, sample)
-    if match:
-        return True
-    else:
-        return False
-
-for word in short_pos_words:
-    if is_word(word):
-        all_words.append(word.lower())
-for word in short_neg_words:
-    if is_word(word):
-        all_words.append(word.lower())
-
-all_words = nltk.FreqDist(all_words)
-
-common_words = all_words.most_common(20)
-common_words_dictionary = dict(common_words)
-stop_words = list(common_words_dictionary.keys())
-
-
-word_features = list(all_words.keys())[:5000]
-print(word_features)
-print(stop_words)
-refined_vocabulary = [word for word in word_features if word not in stop_words]
-
-save_refined_vocabulary = open("refined_vocabulary.pickle", "wb")
-pickle.dump(refined_vocabulary, save_refined_vocabulary)
-save_refined_vocabulary.close()
+# save_refined_vocabulary = open("refined_vocabulary.pickle", "wb")
+# pickle.dump(refined_vocabulary, save_refined_vocabulary)
+# save_refined_vocabulary.close()
 
 def find_features(document):
     words = word_tokenize(document)
@@ -95,3 +99,15 @@ print("Original Naive Bayes accuracy percent:", (nltk.classify.accuracy(classifi
 classifier.show_most_informative_features(15)
 print("BernoulliNB_classifier accuracy percent:", (nltk.classify.accuracy(BernoulliNB_classifier, testing_set))*100)
 print("LogisticRegression_classifier accuracy percent:", (nltk.classify.accuracy(LogisticRegression_classifier, testing_set))*100)
+
+save_LogisticRegression_classifier = open("LogisticRegression.pickle", "wb")
+pickle.dump(LogisticRegression_classifier, save_LogisticRegression_classifier)
+save_LogisticRegression_classifier.close()
+
+save_BernoulliNB_classifier = open("BernoulliNB.pickle", "wb")
+pickle.dump(BernoulliNB_classifier, save_BernoulliNB_classifier)
+save_BernoulliNB_classifier.close()
+
+save_originalNB_classifier = open("original_NB.pickle", "wb")
+pickle.dump(classifier, save_originalNB_classifier)
+save_originalNB_classifier.close()
