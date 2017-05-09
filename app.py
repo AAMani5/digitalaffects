4190b8ad8a8b7c27acc425d9158aac62cb21923c
Ashwini <ashie.oh@gmail.com>
2017-05-09 21:06:45 +0100

    Change server file to be compatiable with pytest-flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_navigation import Navigation
 import requests
 import json
@@ -6,31 +6,38 @@ import pickle
 from model.twitterAPI import getTweets
 from model.NBclassifier import naiveBayesSentimentCalculator
 
-app = Flask(__name__)
-nav = Navigation(app)
+def create_app():
+    app = Flask(__name__)
+    nav = Navigation(app)
 
-nav.Bar('top', [
-    nav.Item('Home', 'index'),
-])
+    nav.Bar('top', [
+        nav.Item('Home', 'index'),
+        ])
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+    @app.route("/")
+    def index():
+        return render_template('index.html')
 
-@app.route("/results", methods=['GET'])
-def results():
-    if request.method == 'GET':
-        text = request.args.get('userinput')
-        tweets = getTweets(text)
-        results = []
+    @app.route("/results", methods=['GET'])
+    def results():
+        if request.method == 'GET':
+            text = request.args.get('userinput')
+            tweets = getTweets(text)
+            results = []
 
-        for tweet in tweets:
-            result = naiveBayesSentimentCalculator(tweet)
-            results.append(result)
-        values = [results.count('positive'), results.count('negative')]
-        return render_template('results.html', values=values, text=text, tweets=tweets)
+            for tweet in tweets:
+                result = naiveBayesSentimentCalculator(tweet)
+                results.append(result)
+            values = [results.count('positive'), results.count('negative')]
+            return render_template('results.html', values=values, text=text, tweets=tweets)
 
+    @app.route('/ping')
+    def ping():
+        return jsonify(ping='pong')
+
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
     app.debug = True
     app.run()
